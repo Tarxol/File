5d42b9bbebb16adb44ed73032876d0bdd2bc3c72
Tarxol <tarxol@yandex.ru>
2022-03-23 03:31:30 +0300

    2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 cook_book = {}
 with open('recipes.txt', encoding='utf-8') as file:
-    while True
-    x = file.readline().strip()
-    cook_book[x] = []
-    for y in range(int(file.readline().strip())):
-        y = file.readline().strip().split("|")
-        z = {}
-        z['ingredient_name'] = y[0]
-        z['quantity'] = y[1]
-        z['measure'] = y[2]
-        cook_book[x] += [z]
-    print(cook_book)
-
+    while True:
+        x = file.readline().strip()
+        if not x:
+            break
+        else:
+            cook_book[x] = []
+            for b in range(int(file.readline().strip())):
+                y = file.readline().strip().split("|")
+                z = {}
+                z['ingredient_name'] = y[0]
+                z['quantity'] = y[1]
+                z['measure'] = y[2]
+                cook_book[x] += [z]
+            file.readline()
+print(cook_book)
